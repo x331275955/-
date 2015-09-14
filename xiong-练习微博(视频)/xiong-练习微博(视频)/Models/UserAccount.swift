8c418be31641bb7ee009ab382a18bcf4d83b1547
王晨阳 <331275955@qq.com>
2015-09-14 15:43:28 +0800

    判断userAccount是否合法

---
+++ xiong-练习微博(视频)/xiong-练习微博(视频)/Models/UserAccount.swift
@@ -0,0 +1,13 @@
+//
+//  UserAccount.swift
+//  xiong-练习微博(视频)
+//
+//  Created by 王晨阳 on 15/9/14.
+//  Copyright © 2015年 IOS. All rights reserved.
+//
+
+import UIKit
+
+class UserAccount: NSObject {
+
+}
---
+++ xiong-练习微博(视频)/xiong-练习微博(视频)/NetworkTools.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkTools.swift
+//  xiong-练习微博(视频)
+//
+//  Created by 王晨阳 on 15/9/13.
+//  Copyright © 2015年 IOS. All rights reserved.
+//
+
+import UIKit
+
+class NetworkTools: NSObject {
+
+}
